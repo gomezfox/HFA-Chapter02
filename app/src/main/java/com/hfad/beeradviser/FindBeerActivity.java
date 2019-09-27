57e8c65e0f7fe72c4bb7d57df98e71f8c5e8dfdc
gomezfox <stephen@gomezfox.net>
2019-09-26 19:57:43 -0700

    Refactor to use anonymous function

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -3,6 +3,8 @@ package com.hfad.beeradviser;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,32 +17,31 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
-    }
+        Button button = (Button) findViewById(R.id.find_beer);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get a reference to the TextView
+                TextView brands = (TextView) findViewById(R.id.brands);
 
-    //Call when the button gets clicked
-    public void onClickFindBeer(View view) {
-        //Get a reference to the TextView
-        TextView brands = (TextView) findViewById(R.id.brands);
+                //Get a reference to the Spinner
+                Spinner color = (Spinner) findViewById(R.id.color);
 
-        //Get a reference to the Spinner
-        Spinner color = (Spinner) findViewById(R.id.color);
+                //Get the selected item in the Spinner
+                String beerType = String.valueOf(color.getSelectedItem());
 
-        //Get the selected item in the Spinner
-        String beerType = String.valueOf(color.getSelectedItem());
+                //TODO: Get recommendations from the BeerExpert class
+                //TODO: Display the brands instead of the beerType using the BeerExpert class
 
-        //TODO: Get recommendations from the BeerExpert class
-        //DONE:  Unnecessary here, incorporated into the for loop below
+                //DONE:  Iterate through List of brands and build a multiline string
+                StringBuilder allBrands = new StringBuilder();
+                for (String brandItem:expert.getBrands(beerType))
+                {
+                    allBrands.append(brandItem).append("\n");
+                }
 
-        //TODO: Display the brands instead of the beerType using the BeerExpert class
-        //DONE:  Iterate through brandList and build a multiline string
-        StringBuilder allBrands = new StringBuilder();
-        for (String brandItem:expert.getBrands(beerType))
-        {
-            allBrands.append(brandItem);
-            allBrands.append("\n");
-        }
-
-        brands.setText(allBrands);
-
+                brands.setText(allBrands);
+            }
+        });
     }
 }
